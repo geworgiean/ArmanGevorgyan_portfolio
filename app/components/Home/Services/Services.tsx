11c71842fbfbb33a1c10e9609cda0f4275e1a61f
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-05 22:07:41 +0400

    finished Services

---
+++ app/components/Home/Services/Services.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ServicesCard from './ServicesCard'
+
+const Services = () => {
+  return (
+    <div className="pt-16 pb-16">
+       <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+        Colleborate with brand <br />and agencies to create <br /> impactful results
+        </h1>
+        <div className='w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols4 gap-10 mt-20
+        items-center'>
+            <div>
+                <ServicesCard icon="/images/s1.png" 
+                name="UI and UX" 
+                description="Designing interfaces that are intuitive, efficient, and enjoyable to use." />
+            </div>
+            <div>
+                <ServicesCard icon="/images/s2.png" 
+                name="Web and Mobile APP" 
+                description="Designing interfaces that are intuitive, efficient, and enjoyable to use." />
+            </div>
+            <div>
+                <ServicesCard icon="/images/s3.png" 
+                name="Design & Creative" 
+                description="Designing interfaces that are intuitive, efficient, and enjoyable to use." />
+            </div>
+            <div>
+                <ServicesCard icon="/images/s4.png" 
+                name="Development" 
+                description="Designing interfaces that are intuitive, efficient, and enjoyable to use." />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Services
