e52eba88cf73d952d828af7c7c03608f0ea9d726
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-05 12:32:20 +0400

    added Navbar

---
+++ app/components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="overflow-hidden">home</div>
+)};
+
+export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Home from "./components/Home/Home";
 
 const HomePage = () => {
-  return <div>Home Page</div>
+  return <Home />;
 };
 
 export default HomePage;
+
+
