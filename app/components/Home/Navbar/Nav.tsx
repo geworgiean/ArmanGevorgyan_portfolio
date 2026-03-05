c7af2ca131033d3662b45727e6b5879dee1bfbbd
Arman Gevorgyan <geworgiean@gmail.com>
2026-03-05 15:13:40 +0400

    finished Navbar

--- app/components/Home/Navbar/Nav.tsx
+++ app/components/Home/Navbar/Nav.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react'
 import { FaCode } from 'react-icons/fa';
+import Link from 'next/link';
+import { NavLinks } from '@/constant/constant';
+import { BiDownload } from 'react-icons/bi';
+import { HiBars3BottomRight } from 'react-icons/hi2';
 
-const Nav = () => {
+
+type Props = {
+    openNav:() => void;
+};
+
+
+const Nav = ({openNav}:Props) => {
+    const [navBg, setNavBg] = useState(false);
+
+    useEffect(() => {
+        const handler=()=>{
+            if(window.scrollY >= 90) setNavBg(true);
+            if(window.scrollY < 90) setNavBg(false);
+            };
+
+            window.addEventListener("scroll", handler);
+
+            return ()=> window.removeEventListener("scroll", handler);
+    },[])
   return (
-    <div className="transition-all duration-200 h-[12vh] z-[10000] fixed w-full" >
+    <div className={`transition-all ${navBg ? "bg-[#0f142ed9] shadow-md" : "fixed"} 
+                    duration-200 h-[12vh] z[10000] fixed w-full`} >
         <div className="flex items-center h-full justify-between w-[90%] mx-auto ">
             {/* Logo */}
             <div className="flex items-center space-x-2">
@@ -12,8 +36,36 @@ const Nav = () => {
                 </div>
                 <h1 className='text-xl hidden sm:block md:text:2xl text-white font-bold'>Arman Gevorgyan</h1>
             </div>
+            {/* navlinks */}
+            <div className='hidden lg:flex items-center space-x-10'>
+                {NavLinks.map((link)=>{
+                    return (
+                    <Link 
+                    key={link.id} 
+                    href={link.url} 
+                    className="text base hover:text-pink-600 text-white font-medium transition-all duration-200"
+                    >
+                        <p>{link.label}</p>
+                    </Link>
+                    );
+                })}
+                </div>
+                {/* buttons */}
+                <div className='flex items-center space-x-4'>
+                            {/* CV BUTTON */}
+                            <button 
+                                className={`px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-pink-800 hover:bg-pink-900
+                                transition-all duration-300 text-white flex items-center space-x-2`}>
+                                <BiDownload className='w-5 h-5'/>
+                                <span>Download CV</span> 
+                            </button>
+                    {/* Burger Menu */}
+                    <HiBars3BottomRight 
+                    onClick={openNav} 
+                    className='w-8 h-8 cursor-pointer text-white lg:hidden'/>
+                </div>
+            </div>
         </div>
-    </div>
   )
 }
 
